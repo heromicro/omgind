7f7bbb3dc9b01acc88e6417f115e6dcac7602778
hello <keye.myria@gmail.com>
2023-03-02 10:57:28 +0800

    add generator for entgo

---
+++ cmd/generator/entgo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+
+	err := entc.Generate("./internal/schema/entity", &gen.Config{
+		Features: []gen.Feature{
+			//gen.FeatureEntQL,
+			//gen.FeatureSnapshot,
+			gen.FeatureSchemaConfig,
+			gen.FeatureLock,
+			gen.FeatureModifier,
+			gen.FeatureUpsert,
+		},
+		Package: "kuiper/internal/gen/ent",
+		Target:  "./internal/gen/ent",
+		Templates: []*gen.Template{
+			gen.MustParse(gen.NewTemplate("mutation_input").ParseDir("./internal/schema/template")),
+		},
+	})
+
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
