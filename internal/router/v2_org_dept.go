4c206e892cb8be0a189af2996a2d6c356aaf25c6
hello <keye.myria@gmail.com>
2023-03-23 16:21:25 +0800

    dept

---
+++ internal/router/v2_org_dept.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	api_v2 "github.com/heromicro/omgind/internal/api/v2"
+)
+
+func (r *Router) initOrgDepartmentRouterV2(urg *gin.RouterGroup, api *api_v2.OrgDepartment, pathcomp string) {
+	gOrgDept := urg.Group(pathcomp)
+	{
+		gOrgDept.GET("", api.Query)
+		gOrgDept.GET(":id", api.Get)
+		gOrgDept.POST("", api.Create)
+		gOrgDept.PUT(":id", api.Update)
+		gOrgDept.DELETE(":id", api.Delete)
+		gOrgDept.PATCH(":id/enable", api.Enable)
+		gOrgDept.PATCH(":id/disable", api.Disable)
+		gOrgDept.GET(":id/view", api.View)
+		gOrgDept.GET(":id/subs", api.GetAllSubs)
+		gOrgDept.GET(":id/tree", api.QueryTree)
+	}
+}
