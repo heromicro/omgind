040deaa89b16abace4ebefc449a6f85286fec8e9
hello <keye.myria@gmail.com>
2023-06-09 16:13:07 +0800

    Update common.go

--- internal/schema/repo/common.go
+++ internal/schema/repo/common.go
@@ -4,6 +4,32 @@ import (
 	"entgo.io/ent/dialect/sql"
 )
 
+type OrderDire string
+
+const (
+	// OrderByASC 升序排序
+	OrderByASC OrderDire = "asc"
+	// OrderByDESC 降序排序
+	OrderByDESC OrderDire = "desc"
+)
+
+func (o OrderDire) String() string {
+	switch o {
+	case OrderByASC:
+		return "asc"
+	case OrderByDESC:
+		return "desc"
+	default:
+		return ""
+	}
+}
+
+// OrderField 排序字段
+type OrderField struct {
+	Key       string    // 字段名(字段名约束为小写蛇形)
+	Direction OrderDire // 排序方向
+}
+
 func OrderDirection(v string) sql.OrderTermOption {
 	switch v {
 	case "a":
